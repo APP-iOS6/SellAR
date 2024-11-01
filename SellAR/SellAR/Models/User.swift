bb7420e2c951e92e8585633770541dfa3b7c0ce3
Juno Lee <juno7746@naver.com>
2024-11-01 10:52:37 +0900

    [Add] : 모델링 파일 추가

---
+++ SellAR/SellAR/Models/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  SellAR
+//
+//  Created by Juno Lee on 11/1/24.
+//
+
+import SwiftUI
+
+struct User: Identifiable {
+    var id: String
+    var email: String
+    var username: String
+    var profileImageUrl: String?
+    
+}
---
+++ SellAR/SellAR/Views/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  SellAR
+//
+//  Created by Juno Lee on 11/1/24.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainView()
+}
