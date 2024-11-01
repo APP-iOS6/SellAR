fdfe413596649f4cfd9c65e9735451b09f0628b0
Min <jmsj0308@naver.com>
2024-11-01 16:38:47 +0900

    [Feat]: SalesStatusVIew 상품 상태 변경 버튼 추가 및 버튼 클릭하면 시트 내려가는 기능 추가

--- SellAR/SellAR/Views/SalesStatusView.swift
+++ SellAR/SellAR/Views/SalesStatusView.swift
@@ -8,11 +8,44 @@
 import SwiftUI
 
 struct SalesStatusView: View {
+    @Environment(\.dismiss) var dismiss
+    @Environment(\.colorScheme) var colorScheme
+    
+    @Binding var showSalesStatusSheet: Bool
+    @Binding var showDetail: Bool
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Button(action: {
+                showDetail = false
+                print("판매 중")
+            }) {
+                Text("판매 중")
+                    .foregroundColor(colorScheme == .dark ? Color.white : Color.black)
+            }
+            .padding()
+            
+            Divider()
+            
+            Button(action: {
+                showDetail = false
+                print("판매 완료")
+            }) {
+                Text("판매 완료")
+                    .foregroundColor(colorScheme == .dark ? Color.white : Color.black)
+            }
+            .padding()
+            
+            Divider()
+            
+            Button(action: {
+                showDetail = false
+                print("예약 중")
+            }) {
+                Text("예약 중")
+                    .foregroundStyle(Color.red)
+            }
+            .padding(.top, 10)
+        }
     }
-}
-
-#Preview {
-    SalesStatusView()
 }
