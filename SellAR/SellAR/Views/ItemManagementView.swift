4077aacc962e91ada39df826c82a56a8346b65c9
Min <jmsj0308@naver.com>
2024-11-01 16:30:33 +0900

    [Add]: ItemManagementView 파일 추가

---
+++ SellAR/SellAR/Views/ItemManagementView.swift
@@ -0,0 +1,18 @@
+//
+//  ItemManagementView.swift
+//  SellAR
+//
+//  Created by Min on 11/1/24.
+//
+
+import SwiftUI
+
+struct ItemManagementView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ItemManagementView()
+}
