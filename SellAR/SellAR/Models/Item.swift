62f972f73266aa7e9cbea578659cb2f088621471
Min <jmsj0308@naver.com>
2024-11-04 18:16:26 +0900

    Revert "[Feat]: Model 에서 Item 구조를 변경"
    This reverts commit 83d2b42081bd23188db802d7b2716f1e5ba238e7.

--- SellAR/SellAR/Models/Item.swift
+++ SellAR/SellAR/Models/Item.swift
@@ -5,8 +5,7 @@
 //  Created by Juno Lee on 11/1/24.
 //
 
-import Foundation
-import FirebaseFirestore
+import SwiftUI
 
 struct Item: Identifiable {
     var id: String
@@ -18,32 +17,6 @@ struct Item: Identifiable {
     var category: String
     var location: String
     var isSold: Bool
-    var createdAt: Date
-    var updatedAt: Date
-    var thumbnailLink: String
-    var usdzLink: String
-    
-    init?(document: [String: Any]) {
-        guard let id = document["id"] as? String,
-              let title = document["itemName"] as? String else {
-            return nil
-        }
-        
-        self.id = id
-        self.userId = document["userId"] as? String ?? ""
-        self.title = title
-        self.description = document["description"] as? String ?? ""
-        self.price = Double(document["price"] as? String ?? "0") ?? 0
-        self.images = [(document["thumbnailLink"] as? String ?? "placeholder")]
-        self.category = "카테고리" // 카테고리를 기본값으로 설정
-        self.location = document["location"] as? String ?? ""
-        self.isSold = false
-        self.createdAt = (document["createdAt"] as? Timestamp)?.dateValue() ?? Date()
-        self.updatedAt = (document["updatedAT"] as? Timestamp)?.dateValue() ?? Date()
-        self.thumbnailLink = document["thumbnailLink"] as? String ?? ""
-        self.usdzLink = document["usdzLink"] as? String ?? ""
-    }
-
+    var createdAt: Date 
 }
-
 
