3363c55bd4ca21c4ba27a3931536f963b3705a0f
Min <jmsj0308@naver.com>
2024-11-04 13:10:31 +0900

    [Feat]: ItemEditView에 ScrollView 기능 추가

--- SellAR/SellAR/Views/ItemEditView.swift
+++ SellAR/SellAR/Views/ItemEditView.swift
@@ -17,157 +17,162 @@ struct ItemEditView: View {
     let placeholder: String = "글을 입력해 주세요."
 
     var body: some View {
-        VStack{
-            Text("게시글 수정")
-                .font(.title3)
-                .bold()
-                .foregroundColor(colorScheme == .dark ? Color.white : Color.black)
-
-            
-            Image("macbook")
-                .resizable()
-                .clipped()
-                .frame(width: 250, height: 220)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 10)
-                        .stroke(Color.black, lineWidth: 2)
-                )
-                .padding(.top, 10)
+        ScrollView {
+            VStack{
+                Text("게시글 수정")
+                    .font(.title3)
+                    .bold()
+                    .foregroundColor(colorScheme == .dark ? Color.white : Color.black)
                 
-            HStack {
-                Button(action: {
-                    
-                }) {
-                    Text("촬영하기")
-                    Image(systemName: "camera")
-                    
-                }
-                .foregroundColor(colorScheme == .dark ? Color.white : Color.black)
-                .frame(width: 100, height: 45)
-                .background(Color.gray)
-                .cornerRadius(10)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 10)
-                        .stroke(Color.black, lineWidth: 2)
-                )
-                Spacer()
                 
-                Button(action: {
-                    
-                }) {
-                    Text("올리기")
-                    Image(systemName: "square.and.arrow.up")
-                    
-                }
-                .foregroundColor(colorScheme == .dark ? Color.white : Color.black)
-                .frame(width: 100, height: 45)
-                .background(Color.gray)
-                .cornerRadius(10)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 10)
-                        .stroke(Color.black, lineWidth: 2)
-                )
+                Image("macbook")
+                    .resizable()
+                    .clipped()
+                    .frame(width: 250, height: 220)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color.black, lineWidth: 2)
+                    )
+                    .padding(.top, 10)
                 
-                Spacer()
-                
-                Button(action: {
-                    
-                }) {
-                    Text("이미지")
-                    Image(systemName: "photo")
-                    
-                }
-                .foregroundColor(colorScheme == .dark ? Color.white : Color.black)
-                .frame(width: 100, height: 45)
-                .background(Color.gray)
-                .cornerRadius(10)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 10)
-                        .stroke(Color.black, lineWidth: 2)
-                )
-            }
-            .padding(.top, 10)
-            
-            HStack {
-                TextField("제목을 입력해 주새요", text: $titleTextField)
-                    .frame(maxWidth: .infinity, maxHeight: 25)
-                    .textFieldStyle(.plain)
-                    .focused($textFocused)
-                    .padding(.vertical, 10)
-                    .padding(.leading, 10)
-                    .background(Color(.systemGray5))
+                HStack {
+                    Button(action: {
+                        
+                    }) {
+                        Text("촬영하기")
+                        Image(systemName: "camera")
+                        
+                    }
+                    .foregroundColor(colorScheme == .dark ? Color.white : Color.black)
+                    .frame(width: 100, height: 45)
+                    .background(Color.gray)
                     .cornerRadius(10)
                     .overlay(
                         RoundedRectangle(cornerRadius: 10)
-                            .stroke(Color.gray, lineWidth: 2)
+                            .stroke(Color.black, lineWidth: 2)
                     )
-                
-                Spacer()
-                
-                Button(action: {
+                    Spacer()
                     
-                }) {
-                    Text("지역설정")
-                    Image(systemName: "map")
-                    
-                }
-                .foregroundColor(colorScheme == .dark ? Color.white : Color.black)
-                .frame(width: 100, height: 45)
-                .background(Color.gray)
-                .cornerRadius(10)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 10)
-                        .stroke(Color.black, lineWidth: 2)
-                )
-            }
-            .padding(.top, 10)
-            
-            TextEditor(text: $textEditor)
-                .frame(width: .infinity, height: 220)
-                .cornerRadius(10)
-                .focused($textFocused)
-                .overlay {
-                    Text(placeholder)
-                        .foregroundColor(colorScheme == .dark ? Color.white : Color.black)
-                    
-                    RoundedRectangle(cornerRadius: 10)
-                        .stroke(Color.black, lineWidth: 2)
-                }
-                .padding(.top, 10)
-            
-            
-            HStack {
-                TextField("가격을 입력해 주세요.", text: $priceTextField)
-                    .frame(maxWidth: .infinity, maxHeight: 25)
-                    .textFieldStyle(.plain)
-                    .focused($textFocused)
-                    .padding(.vertical, 10)
-                    .padding(.leading, 10)
-                    .background(Color(.systemGray5))
+                    Button(action: {
+                        
+                    }) {
+                        Text("올리기")
+                        Image(systemName: "square.and.arrow.up")
+                        
+                    }
+                    .foregroundColor(colorScheme == .dark ? Color.white : Color.black)
+                    .frame(width: 100, height: 45)
+                    .background(Color.gray)
                     .cornerRadius(10)
                     .overlay(
                         RoundedRectangle(cornerRadius: 10)
-                            .stroke(Color.gray, lineWidth: 2)
+                            .stroke(Color.black, lineWidth: 2)
                     )
-                
-                Button(action: {
                     
-                }) {
-                    Text("등록")
+                    Spacer()
+                    
+                    Button(action: {
+                        
+                    }) {
+                        Text("이미지")
+                        Image(systemName: "photo")
+                        
+                    }
+                    .foregroundColor(colorScheme == .dark ? Color.white : Color.black)
+                    .frame(width: 100, height: 45)
+                    .background(Color.gray)
+                    .cornerRadius(10)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color.black, lineWidth: 2)
+                    )
                 }
-                .foregroundColor(colorScheme == .dark ? Color.white : Color.black)
-                .frame(width: 100, height: 45)
-                .background(Color.white)
-                .cornerRadius(10)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 10)
-                        .stroke(Color.black, lineWidth: 2)
-                )
-            }
-            .padding(.top, 10)
+                .padding(.top, 10)
                 
-            Spacer()
+                HStack {
+                    TextField("제목을 입력해 주새요", text: $titleTextField)
+                        .frame(maxWidth: .infinity, maxHeight: 25)
+                        .textFieldStyle(.plain)
+                        .focused($textFocused)
+                        .padding(.vertical, 10)
+                        .padding(.leading, 10)
+                        .background(Color(.systemGray5))
+                        .cornerRadius(10)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color.gray, lineWidth: 2)
+                        )
+                    
+                    Spacer()
+                    
+                    Button(action: {
+                        
+                    }) {
+                        Text("지역설정")
+                        Image(systemName: "map")
+                        
+                    }
+                    .foregroundColor(colorScheme == .dark ? Color.white : Color.black)
+                    .frame(width: 100, height: 45)
+                    .background(Color.gray)
+                    .cornerRadius(10)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color.black, lineWidth: 2)
+                    )
+                }
+                .padding(.top, 10)
+                
+                TextEditor(text: $textEditor)
+                    .frame(width: .infinity, height: 220)
+                    .cornerRadius(10)
+                    .focused($textFocused)
+                    .overlay {
+                        Text(placeholder)
+                            .foregroundColor(colorScheme == .dark ? Color.white : Color.black)
+                        
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color.black, lineWidth: 2)
+                    }
+                    .padding(.top, 10)
+                
+                
+                HStack {
+                    TextField("가격을 입력해 주세요.", text: $priceTextField)
+                        .frame(maxWidth: .infinity, maxHeight: 25)
+                        .textFieldStyle(.plain)
+                        .focused($textFocused)
+                        .padding(.vertical, 10)
+                        .padding(.leading, 10)
+                        .background(Color(.systemGray5))
+                        .cornerRadius(10)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color.gray, lineWidth: 2)
+                        )
+                        .keyboardType(.numberPad)
+                    
+                    Button(action: {
+                        
+                    }) {
+                        Text("등록")
+                    }
+                    .foregroundColor(colorScheme == .dark ? Color.white : Color.black)
+                    .frame(width: 100, height: 45)
+                    .background(Color.white)
+                    .cornerRadius(10)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color.black, lineWidth: 2)
+                    )
+                }
+                .padding(.top, 10)
+                
+                Spacer()
+            }
+            .padding(.horizontal, 16)
         }
+        
         .contentShape(Rectangle())
         .onTapGesture {
             textFocused = false
@@ -182,6 +187,5 @@ struct ItemEditView: View {
                 }
             }
         }
-        .padding(.horizontal, 16)
     }
 }
