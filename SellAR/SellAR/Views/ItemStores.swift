64090ac95da248fe4ad4cd4f4ecf8d9ef1fc83d3
Min <jmsj0308@naver.com>
2024-11-05 13:03:37 +0900

    [Add]: ItemStores 파일 추가

--- SellAR/SellAR/Views/ItemStores.swift
+++ SellAR/SellAR/Views/ItemStores.swift
@@ -12,9 +12,14 @@ class ItemStore: ObservableObject {
     @Published var items: [Item] = []
     
     private var db = Firestore.firestore()
-    
+    private var listener: ListenerRegistration?  // 리스너를 위한 변수 추가
+
     func fetchItems() {
-        db.collection("items").getDocuments { (snapshot, error) in
+        // 기존 리스너가 존재할 경우 제거
+        listener?.remove()
+
+        // Firestore에서 실시간 업데이트를 수신하기 위해 리스너를 추가합니다.
+        listener = db.collection("items").addSnapshotListener { [weak self] (snapshot, error) in
             if let error = error {
                 print("Error fetching documents: \(error)")
                 return
@@ -25,14 +30,18 @@ class ItemStore: ObservableObject {
                 return
             }
             
-            self.items = documents.compactMap { queryDocumentSnapshot -> Item? in
+            self?.items = documents.compactMap { queryDocumentSnapshot -> Item? in
                 let data = queryDocumentSnapshot.data()
                 print("Document data: \(data)")  // 데이터를 출력하여 확인
-                return Item(document: data)
+                return Item(document: data)  // Item의 초기화 메서드에 맞춰 데이터 매핑
             }
             
-            print("Items loaded: \(self.items)")  // 로드된 items를 확인
+            print("Items loaded: \(String(describing: self?.items))")  // 로드된 items를 확인
         }
     }
 
+    deinit {
+        // ItemStore가 해제될 때 리스너를 제거합니다.
+        listener?.remove()
+    }
 }
