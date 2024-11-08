292f046c0e839b2887b74cc3f2ce4e599e647982
Min <jmsj0308@naver.com>
2024-11-08 13:15:16 +0900

    [Feat]: ItemListView 유저 아이디로 게시물을 가져오는 기능으로 바꿈

--- SellAR/SellAR/Views/ItemStores.swift
+++ SellAR/SellAR/Views/ItemStores.swift
@@ -14,32 +14,54 @@ class ItemStore: ObservableObject {
     private var db = Firestore.firestore()
     private var listener: ListenerRegistration?  // 리스너를 위한 변수 추가
 
-    // 실시간으로 업데이트되는 fetchItems
-    func fetchItems() {
-        // 기존 리스너가 존재할 경우 제거
-        listener?.remove()
-
-        // Firestore에서 실시간 업데이트를 수신하기 위해 리스너를 추가합니다.
-        listener = db.collection("items").addSnapshotListener { [weak self] (snapshot, error) in
-            if let error = error {
-                print("Error fetching documents: \(error)")
-                return
+    // 유저 아이디로 실시간 업데이트 되는 fetchItem
+    func fetchItems(for userId: String) {
+        db.collection("items")
+            .whereField("userId", isEqualTo: userId)
+            .addSnapshotListener { [weak self] (snapshot, error) in
+                if let error = error {
+                    print("Error fetching documents: \(error)")
+                    return
+                }
+                
+                guard let documents = snapshot?.documents else {
+                    print("No documents found")
+                    return
+                }
+                
+                self?.items = documents.compactMap { queryDocumentSnapshot -> Item? in
+                    let data = queryDocumentSnapshot.data()
+                    return Item(document: data)
+                }
             }
-            
-            guard let documents = snapshot?.documents else {
-                print("No documents found")
-                return
-            }
-            
-            self?.items = documents.compactMap { queryDocumentSnapshot -> Item? in
-                let data = queryDocumentSnapshot.data()
-                print("Document data: \(data)")  // 데이터를 출력하여 확인
-                return Item(document: data)  // Item의 초기화 메서드에 맞춰 데이터 매핑
-            }
-            
-            print("Items loaded: \(String(describing: self?.items))")  // 로드된 items를 확인
-        }
     }
+    
+//     실시간으로 업데이트되는 fetchItems
+//    func fetchItems() {
+//        // 기존 리스너가 존재할 경우 제거
+//        listener?.remove()
+//
+//        // Firestore에서 실시간 업데이트를 수신하기 위해 리스너를 추가합니다.
+//        listener = db.collection("items").addSnapshotListener { [weak self] (snapshot, error) in
+//            if let error = error {
+//                print("Error fetching documents: \(error)")
+//                return
+//            }
+//            
+//            guard let documents = snapshot?.documents else {
+//                print("No documents found")
+//                return
+//            }
+//            
+//            self?.items = documents.compactMap { queryDocumentSnapshot -> Item? in
+//                let data = queryDocumentSnapshot.data()
+//                print("Document data: \(data)")  // 데이터를 출력하여 확인
+//                return Item(document: data)  // Item의 초기화 메서드에 맞춰 데이터 매핑
+//            }
+//            
+//            print("Items loaded: \(String(describing: self?.items))")  // 로드된 items를 확인
+//        }
+//    }
 //    func fetchItems() {
 //            db.collection("items").getDocuments { [weak self] (snapshot, error) in
 //                if let error = error {
