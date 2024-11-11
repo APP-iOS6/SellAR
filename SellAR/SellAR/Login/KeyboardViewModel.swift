14a6e9cd0e583ecf9a432e509a14ea1c69078d12
skdmlp <skdmlp@gmail.com>
2024-11-11 14:44:41 +0900

    fix-닉네임,이미지 업로드 구현 및 체크 완료

--- SellAR/SellAR/Login/KeyboardViewModel.swift
+++ SellAR/SellAR/Login/KeyboardViewModel.swift
@@ -8,32 +8,9 @@
 import Combine
 import SwiftUI
 
-// MARK: 키보드 레이아웃이 지오메트리 안밀게 하는 클래스
+// MARK: 키보드 뷰 모델 파일
 final class KeyboardViewModel: ObservableObject {
     @Published var keyboardHeight: CGFloat = 0
-    
-    private var cancellables = Set<AnyCancellable>()
-    
-    init() {
-        setupKeyboardHeightListener()
-    }
-    
-    private func setupKeyboardHeightListener() {
-        let willShow = NotificationCenter.default.publisher(for: UIResponder.keyboardWillShowNotification)
-            .map { notification -> CGFloat in
-                if let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect {
-                    return keyboardFrame.height
-                }
-                return 0
-            }
-        
-        let willHide = NotificationCenter.default.publisher(for: UIResponder.keyboardWillHideNotification)
-            .map { _ in CGFloat(0) }
-        
-        Publishers.Merge(willShow, willHide)
-            .assign(to: \.keyboardHeight, on: self)
-            .store(in: &cancellables)
-    }
 }
 // 키보드 띄우고 다른곳 누를시 내리게 하는 익스텐션
 extension View {
